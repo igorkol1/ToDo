d7a326f1297a5877776d567df0f405b80ef6b137
Igor Kołodziejczyk <igorkolodziejczyk@MacBook-Pro-Igor.local>
2018-12-17 19:51:26 +0100

    Add data binding to login component

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = "testUserName"
+  password = "testPassword"
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLogin(){
+    console.log(this.username)
+    console.log(this.password)
   }
 
 }
