98475b37c6dc71b9f33a6a55598382b03c8a0cbe
Igor Kołodziejczyk <igorkolodziejczyk@MacBook-Pro-Igor.local>
2018-12-23 14:24:15 +0100

    Add route guard service to app routing

--- frontend/todo/src/app/service/route-guard.service.ts
+++ frontend/todo/src/app/service/route-guard.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router:Router,
+              private hardcodedAuthenticationService:HardcodedAuthenticationService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    if(this.hardcodedAuthenticationService.isUserLoggedIn()){
+      return true;
+    }
+    this.router.navigate(['login'])
+    return false;
+  }
+
 }
