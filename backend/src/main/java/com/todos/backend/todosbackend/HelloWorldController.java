134fd4024368558998bc91f46ea3828114c41c96
Igor Kołodziejczyk <igorkolodziejczyk@MacBook-Pro-Igor.local>
2018-12-28 22:45:05 +0100

    Add method with parameter to hello word controller

--- backend/src/main/java/com/todos/backend/todosbackend/HelloWorldController.java
+++ backend/src/main/java/com/todos/backend/todosbackend/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.todos.backend.todosbackend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWordBean helloWordBean() {
         return new HelloWordBean("Hello Word");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWordBean helloWordWithParameter(@PathVariable String name) {
+        return new HelloWordBean("Hello Word "+name);
     }
 }
