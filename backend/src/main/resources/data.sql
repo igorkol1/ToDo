cfcf439afe46f3486b5e418a801150cdbdee538d
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-01-31 18:17:44 +0100

    Add user entity and fill user table with initial data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,7 +8,16 @@ insert into todo(id,username,description,target_date,is_done)
 values (10003,'username','Learn JPA 3', sysdate(),false);
 
 insert into todo(id,username,description,target_date,is_done)
-values (10004,'username','Learn JPA 4', sysdate(),false);
+values (10004,'username1','Learn JPA 4', sysdate(),false);
 
 insert into todo(id,username,description,target_date,is_done)
-values (10005,'username','Learn JPA 5', sysdate(),false);
+values (10005,'username2','Learn JPA 5', sysdate(),false);
+
+insert into user(id,username,password,role)
+values (10001,'username','$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e','Admin');
+
+insert into user(id,username,password,role)
+values (10002,'username1','$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e','Admin');
+
+insert into user(id,username,password,role)
+values (10003,'username2','$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e','Admin');
