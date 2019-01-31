4873a90ef0baa9bf2616c9574942908641f7c461
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-01-31 17:52:36 +0100

    Refactoring: remove basic authentication and customized welcome message part II

--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { WelcomeDataService } from '../service/data/welcome-data.service';
 import { error } from 'util';
 
 @Component({
@@ -15,31 +14,11 @@ name
 welcomeMessageFromService:string
 
   constructor(
-    private route:ActivatedRoute,
-    private service:WelcomeDataService) { }
+    private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.message = 'Welcome '+this.route.snapshot.params['name']
     this.name = this.route.snapshot.params['name']
     console.log(this.message)
   }
-
-  getWelcomeMessage(){
-    console.log(this.service.executeHelloWordBeanService())
-    this.service.executeHelloWordBeanServiceWithPathVariable(this.name).subscribe(
-      response => this.handleSuccessfulResponse(response),
-      error => this.handleErrorResponse(error)
-    )
-  }
-
-  handleSuccessfulResponse(response){
-    console.log(response.message)
-    this.welcomeMessageFromService=response.message
-  }
-
-  handleErrorResponse(error){
-    this.welcomeMessageFromService = 'Some error has occuired'
-    console.log(error.error.message)
-  }
-
 }
